fd47809a3f2d8e2789477c0e6d99bc9f1296ee63
David Augusto <davidribeiro086@gmail.com>
2022-11-08 17:19:49 -0300

    style: component home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import type { AppProps } from 'next/app'
 import { Roboto } from '@next/font/google'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app';
+
 globalStyles();
 
 const roboto = Roboto({
@@ -10,14 +13,19 @@ const roboto = Roboto({
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Container>
       <style jsx global>{`
         html {
           font-family: ${roboto.style.fontFamily};
         }
       `}</style>
+
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
       <Component {...pageProps} />
-    </>
+    </Container>
   )
 }
 
