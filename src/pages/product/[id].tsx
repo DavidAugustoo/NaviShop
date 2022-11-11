945fd8956a25eeec4b9229cc146d7ed9edf3d951
David Augusto <davidribeiro086@gmail.com>
2022-11-11 17:12:40 -0300

    style: component product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,32 @@
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
 export default function Product() {
-  return <h1>Página de produtos</h1>
+  const { query } = useRouter()
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+          aliquid rerum exercitationem facere a molestiae ut sed velit non
+          mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed
+          laboriosam tempora?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
